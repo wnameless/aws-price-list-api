3b7a6d1449403597bc4a11e24efbc5b69857130d
wmw <wnameless@gmail.com>
2020-06-28 15:38:16 +0800

    Make develop branch to Java 11 release

--- src/test/java/com/github/wnameless/aws/pricelist/api/PriceListApiTest.java
+++ src/test/java/com/github/wnameless/aws/pricelist/api/PriceListApiTest.java
@@ -19,14 +19,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import com.github.wnameless.aws.pricelist.api.model.Offer;
 import com.github.wnameless.aws.pricelist.api.model.OfferIndex;
 import com.github.wnameless.aws.pricelist.api.service.PriceListApiService;
-
-import net.sf.rubycollect4j.Ruby;
 
 public class PriceListApiTest {
 
@@ -36,27 +32,14 @@ public class PriceListApiTest {
     assertTrue(PriceListApi.INSTANCE instanceof PriceListApiService);
   }
 
-  @Disabled
+  // @Disabled
   @Test
   public void testAllApi() throws IOException {
     OfferIndex offerIndex = OfferIndex.get();
 
-    for (AWSOffer awsOffer : AWSOffer.values()) {
-      Offer offer = offerIndex.getOffer(awsOffer);
-
-      Ruby.Array
-          .copyOf(offer.getCurrentProductVersion().getTerms().getOnDemand()
-              .values())
-          .lazy().flatMap(m -> Ruby.Array.copyOf(m.values()))
-          .each(onDemandDetail -> {
-            if (!onDemandDetail.getTermAttributes().isEmpty()) {
-              onDemandDetail.getTermAttributes().entrySet().forEach(e -> {
-                System.out.println(e.getKey().getClass().getName());
-                System.out.println(e.getValue().getClass().getName());
-              });
-            }
-          });
-    }
+    offerIndex.getOffers().values().forEach(offer -> {
+      System.out.println(offer.getOfferCode());
+    });
   }
 
 }
